55f214b7e43b4906b2a9d8eb94dcc77a34d66a2d
axeliono <69375446+axeliono@users.noreply.github.com>
2020-11-27 22:33:11 -0600

    finished 'Employee' parent class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,41 @@
+const Employee = require("../lib/Employee");
+
+test("creates a new employee object", () => {
+  const employee = new Employee("Dave", "jamesbond@gmail.com");
+
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.name.length).toBeGreaterThan(0);
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+  expect(employee.email.length).toBeGreaterThan(0);
+});
+
+test("returns the name property of Employee", () => {
+  const employee = new Employee("James", "jamesbond@gmail.com");
+
+  expect(employee.getName()).toEqual(
+    expect.stringContaining(employee.name.toString())
+  );
+});
+
+test("returns id number of Employee", () => {
+  const employee = new Employee("James", "Jamesbond@gmail.com");
+
+  expect(employee.getId()).toEqual(employee.id);
+});
+
+test("returns email address of Employee", () => {
+  const employee = new Employee("James", "Jamesbond@gmail.com");
+
+  expect(employee.getEmail()).toEqual(
+    expect.stringContaining(employee.email.toString())
+  );
+});
+
+test("returns role which should be 'Employee'", () => {
+  const employee = new Employee("James", "Jamesbond@gmail.com");
+
+  expect(employee.getRole()).toEqual(
+    expect.stringContaining(employee.constructor.name)
+  );
+});
